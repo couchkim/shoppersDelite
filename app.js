c821e939854c0436d5096c4dc85a7c62e9929f20
Kim Hanson <couchkim3800@gmail.com>
2016-12-30 16:21:14 -0500

    Finished product list and styling

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ window.addEventListener('load', function(){
     let products = [
     
     makeProduct('http://cdn.shopify.com/s/files/1/0297/6313/products/seedbeadchokerhavanamoon.jpg?v=1472686292', 'Pura Vida', 'Havana Moon Necklace', 32),
-    // makeProduct('Honda', 'Odyssey', 3, 100),
-    // makeProduct('Honda', 'Odyssey', 3, 100),
-    // makeProduct('Honda', 'Odyssey', 3, 100),
-    // makeProduct('Honda', 'Odyssey', 3, 100),
-    // makeProduct('Honda', 'Odyssey', 3, 100),
+    makeProduct('http://verabradley.scene7.com/is/image/VeraBradley/15460669-V1?$zoom_alt_large$', 'Vera Bradley', 'Large Travel Bag', 85),
+    makeProduct('https://images.lululemon.com/is/image/lululemon/LW7ACQT_0001_3?$gsr-pdt-half-lg$', 'lululemon', 'Running Short', 58),
+    makeProduct('https://cdn.shopify.com/s/files/1/1538/4985/products/745_1_sky_1024x1024.jpg?v=1478891325', 'Balega', 'No Show Sock', 12),
+    makeProduct('http://s7d1.scene7.com/is/image/MoosejawMB/10262254x1012379_zm?$thumb225$', 'Hunter', 'Original Tall Boot', 150),
+    makeProduct('http://img.tennis-warehouse.com/watermark/rs.php?path=PDT-1.jpg&nw=350', 'Babolat', 'Pure Drive Team', 195),
      ];
 
     console.log (products);
@@ -39,29 +39,37 @@ function loadProducts(items){
 
         parent.appendChild(oneItem);
         
+        let imgSection = document.createElement('div');
+        imgSection.setAttribute('id', 'imgSection');
+        oneItem.appendChild(imgSection);
+
+        let productSection = document.createElement('div');
+        productSection.setAttribute('id', 'productSection');
+        oneItem.appendChild(productSection);
+
         let picture = document.createElement('img');
         picture.src = items[i].image;
         picture.setAttribute('id', 'picture');
-        oneItem.appendChild(picture);
+        imgSection.appendChild(picture);
 
         let brand = document.createElement('h2');
         brand.textContent = items[i].name;
         brand.setAttribute('id', 'brand');
-        oneItem.appendChild(brand);
+        productSection.appendChild(brand);
 
         let description = document.createElement('p');
         description.textContent = items[i].description;
         description.setAttribute('id', 'spaces');
-        oneItem.appendChild(description);
+        productSection.appendChild(description);
 
         let cost = document.createElement('p');
         cost.textContent = "$" + items[i].cost;
-        oneItem.appendChild(cost);
+        productSection.appendChild(cost);
 
         let addToCart = document.createElement('button');
         addToCart.textContent = "Add to Cart";
         addToCart.setAttribute('id', 'addToCart');
-        oneItem.appendChild(addToCart);
+        productSection.appendChild(addToCart);
         addToCart.addEventListener('click', function(){
             itemToCart(items[i]);
         });
